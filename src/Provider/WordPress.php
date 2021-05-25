b8c8d893928c14201831fc44a631654d26cb33a5
ProklUng <funtik21975@gmail.com>
2021-05-25 09:49:27 +0300

    Расширение функционала провайдера Wordpress для Faker.

--- src/Provider/WordPress.php
+++ src/Provider/WordPress.php
@@ -2,9 +2,11 @@
 
 namespace Prokl\WordpressCi\Provider;
 
+use Exception;
 use Faker\Provider\Base;
 use Faker\Provider\File;
 use InvalidArgumentException;
+use WP_Query;
 
 /**
  * Class WordPress
@@ -12,11 +14,10 @@ use InvalidArgumentException;
  */
 class WordPress extends Base
 {
-
     /**
      * Get permalink.
      *
-     * @param integer $id
+     * @param integer $id ID поста.
      *
      * @return mixed
      */
@@ -26,9 +27,19 @@ class WordPress extends Base
     }
 
     /**
+     * Фэйковый ID поста.
+     *
+     * @return integer
+     */
+    public function fakeId() : int
+    {
+        return mt_rand(1000000000, 1000000000000);
+    }
+
+    /**
      * Get a file content.
      *
-     * @param string $file
+     * @param string $file Файл.
      *
      * @return string
      */
@@ -119,9 +130,441 @@ class WordPress extends Base
     }
 
     /**
+     * ID случайной картинки.
+     *
+     * @return integer
+     *
+     * @since 26.12.2020
+     */
+    public function randomIdPicture() : int
+    {
+        $args = [
+            'post_type'      => 'attachment',
+            'orderby'        => 'rand',
+            'posts_per_page' => 1,
+            'post_status' => 'published',
+            'nopaging' => false,
+            'no_found_rows' => true,
+        ];
+
+        $query = new WP_Query($args);
+
+        $result = $query->query($args);
+        wp_reset_query();
+
+        return current($result)->ID;
+    }
+
+    /**
+     * URL случайной картинки.
+     *
+     * @return string
+     *
+     * @since 26.12.2020
+     */
+    public function getUrlRandomPicture() : string
+    {
+        $arData = wp_get_attachment_image_src($this->randomIdPicture(), 'full');
+
+        return $arData[0] ?? '';
+    }
+
+    /**
+     * ID случайного поста средствами Wp.
+     *
+     * @param string $postType Тип поста. По умолчанию - post.
+     *
+     * @return integer
+     */
+    public function randomIdPostWp(string $postType = 'post'): int
+    {
+        $args = [
+            'post_type' => $postType,
+            'orderby' => 'rand',
+            'posts_per_page' => 1,
+            'post_status' => 'published',
+            'nopaging' => false,
+            'no_found_rows' => true,
+        ];
+
+        $query = new WP_Query();
+        $result = $query->query($args);
+        wp_reset_query();
+
+        return current($result)->ID;
+    }
+
+    /**
+     * Массовое получение ID случайных постов.
+     *
+     * @param integer $qty  Количество.
+     * @param string  $type Тип поста. По умолчанию - post.
+     *
+     * @return array
+     *
+     * @since 26.12.2020
+     */
+    public function getRandomIdPostMassive(int $qty, string $type = 'post') : array
+    {
+        $result = [];
+        for ($i = 0; $i<$qty; $i++) {
+            $result[] = $this->randomIdPostWp($type);
+        }
+
+        return $result;
+    }
+
+    /**
+     * ID случайной страницы.
+     *
+     * @return integer
+     */
+    public function randomIdPage(): int
+    {
+        return $this->randomIdPostWp('page');
+    }
+
+    /**
+     * Случайный ID поста в категории средствами WP.
+     *
+     * @param string $categorySlug Метка категории.
+     *
+     * @return integer
+     */
+    public function randomIdPostInCategoryWp(string $categorySlug): int
+    {
+        $args = [
+            'post_type' => 'post',
+            'orderby' => 'rand',
+            'posts_per_page' => 1,
+            'post_status' => 'published',
+            'nopaging' => false,
+            'no_found_rows' => true,
+            'category_name' => $categorySlug,
+        ];
+
+        $query = new WP_Query();
+
+        wp_reset_query();
+
+        return current($query->query($args))->ID;
+    }
+
+    /**
+     * ID элемента с контентом.
+     *
+     * @param integer $minLength Минимальная длина поста.
+     * @param string  $postType  Тип поста. По умолчанию - post.
+     *
+     * @return integer
+     */
+    public function getRandomIdPostWithContentWp(
+        int $minLength = 650,
+        string $postType = 'post'
+    ): int {
+        $args = [
+            'post_type' => $postType,
+            'orderby' => 'rand',
+            'posts_per_page' => 1,
+            'post_status' => 'published',
+            'nopaging' => false,
+            'no_found_rows' => true,
+        ];
+
+        $query = new WP_Query();
+
+        do {
+            $result = current($query->query($args));
+        } while (strlen($result->post_content) < $minLength);
+
+        wp_reset_query();
+
+        return $result->ID;
+    }
+
+    /**
+     * ID элемента без контента.
+     *
+     * @param string $postType Тип поста. По умолчанию - post.
+     *
+     * @return integer
+     *
+     * @since 26.12.2020
+     */
+    public function randomIdPostWithoutContentWp(string $postType = 'post'): int
+    {
+        $args = [
+            'post_type' => $postType,
+            'orderby' => 'rand',
+            'posts_per_page' => 1,
+            'post_status' => 'published',
+            'nopaging' => false,
+            'no_found_rows' => true,
+        ];
+
+        $query = new WP_Query();
+
+        do {
+            $result = current($query->query($args));
+        } while ($result->post_content !== '');
+
+        wp_reset_query();
+
+        return $result->ID;
+    }
+
+    /**
+     * ID поста с картинкой.
+     *
+     * @param string $postType   Тип поста.
+     * @param array  $categories Категории.
+     *
+     * @return integer
+     */
+    public static function randomIdPostPictureWp(
+        string $postType = 'post',
+        array $categories = []
+    ): int {
+        $args = [
+            'post_type' => $postType,
+            'orderby' => 'rand',
+            'posts_per_page' => 1,
+            'post_status' => 'published',
+            'nopaging' => false,
+            'no_found_rows' => true,
+            'meta_query' => [
+                [
+                    'key' => '_thumbnail_id',
+                ],
+            ],
+            'category__in' => $categories
+        ];
+
+        $query = new WP_Query();
+        $result = $query->query($args);
+
+        wp_reset_query();
+
+        return current($result)->ID;
+    }
+
+    /**
+     * ID поста без картинки.
+     *
+     * @param string $postType Тип поста.
+     *
+     * @return integer
+     */
+    public function getRandomIdPostWithoutPictureWp(string $postType = 'post'): int
+    {
+        $args = [
+            'post_type' => $postType,
+            'orderby' => 'rand',
+            'posts_per_page' => 1,
+            'post_status' => 'published',
+            'nopaging' => false,
+            'no_found_rows' => true,
+            'meta_query' => [
+                [
+                    'key' => '_thumbnail_id',
+                    'value' => '?',
+                    'compare' => 'NOT EXISTS'
+                ],
+            ],
+        ];
+
+        $query = new WP_Query();
+        $result = $query->query($args);
+
+        return current($result)->ID;
+    }
+
+    /**
+     * ID случайной категории.
+     *
+     * @return integer
+     * @throws Exception
+     */
+    public function randomCategoryId(): int
+    {
+        $categories = get_categories();
+        do {
+            $index = random_int(0, count($categories));
+
+            $randomCategory = $categories[$index] ?? null;
+        } while ($randomCategory->cat_ID === null);
+
+        return $randomCategory->cat_ID;
+    }
+
+    /**
+     * Случайный пост по мета полю.
+     *
+     * @param string $metaKey   Мета поле.
+     * @param mixed  $metaValue Значение.
+     *
+     * @return integer
+     *
+     * @since 26.12.2020
+     */
+    public function getRandomIdByMetaValue(
+        string $metaKey,
+        $metaValue
+    ): int {
+        $args = [
+            'post_type' => 'post',
+            'orderby' => 'rand',
+            'posts_per_page' => 1,
+            'post_status' => 'published',
+            'nopaging' => false,
+            'no_found_rows' => true,
+            'meta_query' => [
+                [
+                    'key' => $metaKey,
+                    'value' => $metaValue,
+                    'compare' => '='
+                ],
+            ],
+        ];
+
+        $query = new WP_Query();
+        $result = $query->query($args);
+
+        return current($result)->ID;
+    }
+
+    /**
+     * Случайный пост по мета полю. Отрицание.
+     *
+     * @param string $metaKey Мета поле.
+     *
+     * @return integer
+     *
+     * @since 26.12.2020
+     */
+    public function getRandomIdByNotMetaValue(
+        string $metaKey
+    ): int {
+        $args = [
+            'post_type' => 'post',
+            'orderby' => 'rand',
+            'posts_per_page' => 1,
+            'post_status' => 'published',
+            'nopaging' => false,
+            'no_found_rows' => true,
+            'meta_query' => [
+                [
+                    'key' => $metaKey,
+                    'compare' => 'NOT EXISTS'
+                ],
+            ],
+        ];
+
+        $query = new WP_Query();
+        $result = $query->query($args);
+
+        return current($result)->ID;
+    }
+
+    /**
+     * Слаг тэга с картинками.
+     *
+     * @return string
+     *
+     * @since 26.12.2020
+     */
+    public function tagWithTagPicture() : string
+    {
+        return get_post_field('post_title', $this->randomIdPostPictureWp( 'tagspicture'));
+    }
+
+    /**
+     * Случайный тэг без картинки.
+     *
+     * @return string
+     * @throws Exception
+     *
+     * @since 26.12.2020
+     */
+    public function getRandomTagSlugWithoutPicture() : string
+    {
+        do {
+            $slug = $this->randomTagSlug();
+
+            $args = [
+                'post_type' => 'tagspicture',
+                'orderby' => 'rand',
+                'posts_per_page' => 1,
+                'post_status' => 'published',
+                'post_title' => $slug,
+                'nopaging' => false,
+                'no_found_rows' => true,
+                'meta_query' => [
+                    [
+                        'key' => '_thumbnail_id',
+                    ],
+                ],
+            ];
+
+            $query = new WP_Query();
+            $result = $query->query($args);
+
+            wp_reset_query();
+        } while (current($result)->ID === null);
+
+        return $slug;
+    }
+
+    /**
+     * Случайный тэг.
+     *
+     * @return string
+     * @throws Exception
+     */
+    public function randomTagSlug() : string
+    {
+        $alltags = get_tags();
+
+        return $alltags[random_int(0, count($alltags))]->slug;
+    }
+
+    /**
+     * Случайный ID поста с заполненным ACF полем video_element.
+     *
+     * @return integer
+     *
+     * @since 29.12.2020
+     */
+    public function idPostWithAcfVideo() : int
+    {
+        $args = [
+            'post_type' => 'post',
+            'orderby' => 'rand',
+            'posts_per_page' => 1,
+            'post_status' => 'published',
+            'nopaging' => false,
+            'no_found_rows' => true,
+            'meta_query' => [
+                [
+                    'key' => 'video_element',
+                    'value' => '',
+                    'compare' => '<>'
+                ],
+            ],
+        ];
+
+        $query = new WP_Query();
+        $result = $query->query($args);
+
+        wp_reset_query();
+
+        return current($result)->ID ?: 0;
+    }
+
+    /**
      * Get a random user ID.
      *
-     * @param array|string $args
+     * @param array|string $args Аргументы.
      *
      * @return integer|boolean
      */
